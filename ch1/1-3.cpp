411d6730e401e7e5b66be4dfd3f34e5b2d4c8fb9
qinyihao <40647091+qinyihao@users.noreply.github.com>
2021-03-08 16:13:55 +0800

    Create 1-3.cpp

---
+++ ch1/1-3.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	cout << 14 / 4 << endl;
+	cout << 14 / 4 * 4 << endl;
+	cout << 14 - 14 / 4 * 4 << endl;
+//	cout << 14 % 4 << endl;
+	return 0;
+}
